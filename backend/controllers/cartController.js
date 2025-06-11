888637a59fdd01e9124d4f032b8a6370ef5fafc4
tanvirtalukdersiam <149420877+tanvirtalukdersiam@users.noreply.github.com>
2025-06-11 13:14:57 +0600

    all

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,46 @@
+import userModel from "../models/userModel.js";
+
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findOne({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, {
+      cartData,
+    });
+    res.json({ success: true, message: " Added To Cart" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: " Error" });
+  }
+};
+const removeToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed From Cart" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Error" });
+  }
+};
+const getCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    res.json({ success: true, data: cartData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Error" });
+  }
+};
+
+export { addToCart, removeToCart, getCart };
---
+++ backend/controllers/orderController.js
@@ -0,0 +1,5 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
+const placeOrder = async (req, res) => {};
+export { placeOrder };
